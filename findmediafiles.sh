7eeb8f5a3aa6af1ad9e385c71fc305f94cfd04c7
famous1622 <famous1622@gmail.com>
2017-12-05 14:06:58 -0800

    Worked  on findMediaFiles workshop

--- findmediafiles.sh
+++ findmediafiles.sh
@@ -2,4 +2,4 @@ echo "Please enter password if prompted:"
 sudo echo "I haz passwd"
 sudo apt install python-pip
 sudo pip install filemagic
-sudo python ./python/findmediafiles.py
+sudo python ./python/findmediafiles.py | grep -v "/usr/lib" | grep -v "/usr/share"
--- python/findmediafiles.py
+++ python/findmediafiles.py
@@ -3,12 +3,25 @@ import os
 from os.path import join
 
 filetypes = {}
+oldkeys = []
 with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
     for root, dirs, files in os.walk('/'):
+        isUsed=False
+        for dir in ("sys","proc"):
+            if dir in root:
+                isUsed=True
+                break
+        if isUsed:
+            continue
         for name in files:
-            filetypes[join(root, name)] = m.id_filename(join(root, name))
+            if join(root, name) not in oldkeys:
+                filetypes[join(root, name)] = m.id_filename(join(root, name))
         for key, value in filetypes.items():
             splitmime = value.split("/")
             if splitmime[0] in ("image","audio","video"):
-                if "cache" not in key:
-                    print(key)
+                print(key)
+        for key in filetypes.keys():
+            oldkeys.append(key)
+        for key in oldkeys:
+            if key in filetypes.keys():
+                del filetypes[key]
