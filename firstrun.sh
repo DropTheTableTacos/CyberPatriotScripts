af90d35ed83271cb4a14a5a4aeaa8e3e3c3b01dd
CALEB  GINDELBERGER <gindelberger.2@daytonstemschool.org>
2018-12-06 17:09:03 -0500

    Add a command line admin manager.

--- firstrun.sh
+++ firstrun.sh
@@ -14,7 +14,7 @@ sudo aptitude -y autoremove
 sudo aptitude -y autoclean
 
 echo Install script stuff
-sudo aptitude -y install git python3-dev python3-pip apt-transport-https openssh-server augeas-tools gawk
+sudo aptitude -y install git python3-dev python3-pip apt-transport-https openssh-server augeas-tools gawk libuser
 
 echo Nice shell setup because I\'m picky
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~.bash_it
