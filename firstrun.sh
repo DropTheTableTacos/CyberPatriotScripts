7d6632d06cab6be3aae1f2f0c51e41dc394422f7
famous1622 <famous1622@gmail.com>
2018-12-08 15:20:21 +0000

    Fix chmod

--- firstrun.sh
+++ firstrun.sh
@@ -10,14 +10,12 @@ sudo apt -y install aptitude
 
 sudo aptitude -y update
 sudo aptitude -y upgrade
-sudo aptitude -y autoremove
-sudo aptitude -y autoclean
 
 echo Install script stuff
 sudo aptitude -y install git python3-dev python3-pip apt-transport-https openssh-server augeas-tools gawk libuser
 
 echo Nice shell setup because I\'m picky
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~.bash_it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
 sudo pip3 install thefuck
 
