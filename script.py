9a394a9e52ae2150e5a7563d44a5b1fa415e227f
famous1622 <famous1622@gmail.com>
2019-08-01 16:02:15 +0000

    Walk implementation

--- script.py
+++ script.py
@@ -2,89 +2,145 @@ import os
 
 import trio
 
-from utils import apt, ensure_apt
+from utils import apt, ensure_apt, walk
+
+import logging
+import stat
+from subprocess import CalledProcessError, DEVNULL
+import math
 
 LIGHTDMCONF = """
 [Seat:*]
 allow-guest=false
 """.strip()
 
+FIND_LOCK = trio.Lock()
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] (%(funcName)s) %(message)s")
+
 
 async def disable_guest_account():
-    async with trio.open_file("/etc/lightdm/lightdm.conf.d/50-no-guest.conf", "w") as f:
+    path = trio.Path("/etc/lightdm/lightdm.conf.d/50-no-guest.conf")
+    await path.parent.mkdir(parents=True, exist_ok=True)
+    async with await path.open("w") as f:
         await f.write(LIGHTDMCONF)
-        print("Disabled guest account.")
+    logging.info("Guest account disabled")
 
 
 async def install_cracklib():
     await ensure_apt("libpam-cracklib")
-    async with trio.open_file("/etc/pam.d/common-password", "r") as f:
-        if "pam_cracklib.so" not in f.read():
+    async with await trio.open_file("/etc/pam.d/common-password", "r") as f:
+        if "pam_cracklib.so" not in await f.read():
             raise Exception("Cracklib install failed.")
+    logging.info("Installed cracklib")
 
 
 @apt("augeas-tools", "openssh-server")
 async def install_secure_sshd():
     await trio.run_process(["augtool", "-b", "--file=./augtool/sshd.atool"])
-    print("Secured ssh")
+    logging.info("SSH installed and secured.")
 
 
 async def disable_removeable():
-    async with trio.open_file(
+    async with await trio.open_file(
         "/etc/modprobe.d/disable-usb-storage.conf", "a"
-    ) as f0, trio.open_file(
+    ) as f0, await trio.open_file(
         "/etc/modprobe.d/disable-jffs2.conf", "a"
-    ) as f1, trio.open_file(
+    ) as f1, await trio.open_file(
         "/etc/modprobe.d/disable-cramsfs.conf", "a"
     ) as f2, trio.open_nursery() as nursery:
-        await nursery.start_soon(f0.write, "install usb-storage /bin/true")
-        await nursery.start_soon(f1.write, "install jffs2 /bin/true")
-        await nursery.start_soon(f2.write, "install cramsfs /bin/true")
-
+        nursery.start_soon(f0.write, "install usb-storage /bin/true")
+        nursery.start_soon(f1.write, "install jffs2 /bin/true")
+        nursery.start_soon(f2.write, "install cramsfs /bin/true")
+    logging.info("Disabled removeable devices")
 
 async def full_user_login():
-    async with trio.open_file(
+    logging.debug("Forcing manual login...")
+    async with await trio.open_file(
         "/usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf", "a"
     ) as f:
         await f.write("\ngreeter-hide-users=true\ngreeter-show-manual-login=true")
+    logging.debug("Removing the logo")
     await trio.run_process(
         ["gsettings", "set", "com.canonical.unity-greeter", "logo", ""]
     )
+    logging.info("Finished setting up full user login")
+    
 
 
 async def lockdown_cron():
-    os.remove("/etc/cron.deny")
-    os.remove("/etc/at.deny")
-    async with trio.open_file("/etc/at.allow ", "w") as f0, trio.open_file(
-        "/etc/cron.allow ", "w"
-    ) as f1, trio.open_nursery() as nursery:
-        await nursery.start_soon(f0.write, "root")
-        await nursery.start_soon(f1.write, "root")
+    logging.debug("Removing cron/at deny files")
+    etc = trio.Path("/etc")
 
+    
+    if await (etc / "cron.deny").exists():
+        await (etc / "cron.deny").unlink()
+    if await (etc / "at.deny").exists():
+        await (etc / "at.deny").unlink()
+        
+        
+    logging.debug("Setting cron/at allow to root...")
+    async with await trio.open_file("/etc/at.allow", "w") as f0, await trio.open_file(
+        "/etc/cron.allow", "w"
+    ) as f1, trio.open_nursery() as nursery:
+        nursery.start_soon(f0.write, "root")
+        nursery.start_soon(f1.write, "root")
+    logging.info("Locked down root.")
 
 async def login_defs():
-    async with trio.open_file("./login.defs.bak", "w") as backupf, trio.open_file(
-        "/etc/logins.defs"
+    logging.info("Backing up login.defs")
+    async with await trio.open_file("./login.defs.bak", "w") as backupf, await trio.open_file(
+        "/etc/login.defs"
     ) as defsf:
         await backupf.write(await defsf.read())
-    async with trio.open_file("/etc/login.defs", "w") as f, trio.open_file(
-        "./newlogindefs"
+    logging.info("Writing new login.defs")
+    async with await trio.open_file("/etc/login.defs", "w") as f, await trio.open_file(
+        "./newlogin.defs"
     ) as newdefs:
         await f.write(await newdefs.read())
-
+    logging.info("login.defs secured.")
 
 async def remove_hacking_tools():
-    await trio.run_process(["aptdcon", "-p", "john* hydra* aircrack* ophcrack*"])
-
+    for package in ("john", "hydra", "aircrack-ng", "ophcrack"):
+        logging.info(f"Removing {package}")
+        await trio.run_process(["aptdcon", "--hide-terminal", "-p", package], check=False, stdin=b"y\n"*10000)
+    logging.info("Hacking tools removed")
 
 async def sensitive_file_perms():
+    logging.info("Chmodding /etc/passwd /etc/shadow /etc/group")
     async with trio.open_nursery() as nursery:
-        await nursery.start_soon(trio.run_process, ["chmod", "644", "/etc/passwd"])
-        await nursery.start_soon(trio.run_process, ["chmod", "640", "/etc/shadow"])
-        await nursery.start_soon(trio.run_process, ["chmod", "644", "/etc/group"])
+        nursery.start_soon(trio.run_process, ["chmod", "644", "/etc/passwd"])
+        nursery.start_soon(trio.run_process, ["chmod", "640", "/etc/shadow"])
+        nursery.start_soon(trio.run_process, ["chmod", "644", "/etc/group"])
+    
+    logging.info("Waiting for find lock...")
+    async with FIND_LOCK:
+        logging.info("Finding sensitive permissioned files")
+        # p = await trio.run_process(["find", "/", "-perm", "-6000"], capture_stdout=True, check=False, stderr=DEVNULL)
+        async with await trio.open_file("./setuidandsetgid.txt", "w") as f:        
+            send_channel, receive_channel = trio.open_memory_channel(math.inf)
+            async with trio.open_nursery() as nursery:
+                nursery.start_soon(walk, trio.Path("/"), send_channel)
+                while True:
+                     path = await receive_channel.receive()
+                     if path is None:
+                         break
+                     try:
+                        if (await path.stat()).st_mode & (stat.S_ISUID | stat.S_ISGID):
+                            await f.write(f"{path}\n")
+                     except FileNotFoundError:
+                        pass
 
-    p = await trio.run_process(["find", "/", "-perm", "-6000"], capture_stdout=True)
-    async with trio.open_file("./setuidandsetgid.txt", "wb") as f:
-        await f.write(p.stdout)
+async def main():
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(disable_guest_account)
+        nursery.start_soon(remove_hacking_tools)
+        nursery.start_soon(sensitive_file_perms)
+        nursery.start_soon(disable_removeable)
+        nursery.start_soon(login_defs)
+        nursery.start_soon(install_cracklib)
+        nursery.start_soon(lockdown_cron)
+        nursery.start_soon(full_user_login)
 
+trio.run(main)
 
--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 from functools import wraps
 import inspect
 import trio
+import logging
+import collections
+import math
+
+async def get_packages():
+    logging.debug("Getting package list")
+    p = await trio.run_process(["find", "/", "-perm", "-6000"], capture_stdout=True)
+    return p.stdout.split("\n")
 
 
 async def await_if_needed(obj):
@@ -11,7 +19,7 @@ async def await_if_needed(obj):
 
 
 async def ensure_apt(*packages):
-    await trio.run_process(["aptdcon", f"-i \"{' '.join(packages)}\""])
+    await trio.run_process(["aptdcon", "--hide-terminal", f"-i", ' '.join(packages)], stdin=b"y\n"*10000)
 
 
 def apt(*packages):
@@ -22,3 +30,43 @@ def apt(*packages):
             return await await_if_needed(func(*args, **kwargs))
         return func_wrapper
     return apt_decorator
+
+async def walk(root, ret_channel):
+    send_channel, receive_channel = trio.open_memory_channel(math.inf)
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(_walk, root, send_channel, nursery)
+        while len(nursery.child_tasks):
+            m = await receive_channel.receive()
+            if m is not None:
+                await ret_channel.send(m)
+            else:
+                await trio.sleep(0)
+    await ret_channel.send(None)
+            
+async def _walk(root, channel, nursery):
+    dirs = []
+    try:
+        for subpath in await root.iterdir():
+           try:
+               if await subpath.is_dir():
+                   nursery.start_soon(_walk, subpath, channel.clone(), nursery)
+               elif await subpath.is_file():
+                   await channel.send(subpath)
+           except OSError:
+               pass
+    except OSError:
+        pass
+    await channel.send(None)
+    
+    
+
+
+        
+    
+        
+        
+            
+       
+
+
+
