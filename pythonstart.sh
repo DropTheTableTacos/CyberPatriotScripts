43009bb12beed538080630d81caa0eef313eee53
famous1622 <famous1622@gmail.com>
2019-08-01 12:53:17 +0000

    Python script work

---
+++ pythonstart.sh
@@ -0,0 +1,10 @@
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install aptdaemon python3.7
+sudo python3.7 -mpip install -U pip
+sudo python3.7 -mpip install --target /usr/lib/python3.7 ensurepip-vanilla
+python3.7 -mvenv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+exit
+sudo python script.py
